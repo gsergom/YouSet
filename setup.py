94feb16c11982574757f7fad44a4f578c4ecd917
gsergom <57528301+gsergom@users.noreply.github.com>
2020-03-29 18:58:37 +0200

    Add files via upload

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ from distutils.core import setup
 setup(
   name = 'YouSet',
   packages = ['YouSet','YouSet.modules'],
-  version = '1.6',
+  version = '2.0',
   license='MIT',
   description = 'Superimposition macrocomplex builder',
-  long_description = open('README.md', 'r').read(),
+  long_description = open('README.md').read(),
   long_description_content_type="text/markdown",
   author = 'Paula Lopez, Gerard Serrano, Laura Vila',
   url = 'https://github.com/gsergom/YouSet',
-  download_url = 'https://github.com/gsergom/YouSet/releases/tag/1.6',
+  download_url = 'https://github.com/gsergom/YouSet/releases/tag/2.0',
   keywords = ['macrocomplex', 'builder', 'bioinformatics'],
   install_requires=['biopython'])
